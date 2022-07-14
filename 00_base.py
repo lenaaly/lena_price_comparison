04aa9e2b0fbf4291c0f1766bd43d84af93d63dcb
lenaaly <101528160+lenaaly@users.noreply.github.com>
2022-07-14 20:23:29 +1200

    base v1 - skeleton of program

---
+++ 00_base.py
@@ -0,0 +1,30 @@
+'''
+
+This is the base component - the program takes the users budget
+and recommends which items are the best value for money, based on
+the weight and price of the item. The program returns the best
+recommended item within the users budget
+
+base v1 - skeleton of program, setting up functions
+
+'''
+
+# setting up functions*****
+
+item_info=[[name_list], [price_list], [weight_list]]
+
+# item name function, takes items name and appends to a list
+
+# item price function, takes item price and appends to a list
+
+# item weight function, takes item weight and appends to a list
+
+# weight g to kg calculation function, converts the weights from g to kg
+
+# budget function, asks the user for the budget and stores in a variable
+
+# *** MAIN ROUTINE ***
+
+# asking the user what their budget is
+
+# asking the user the names of their items
