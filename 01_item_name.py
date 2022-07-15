613df02457844a5bf9038b4620a37ad616f4696d
lenaaly <101528160+lenaaly@users.noreply.github.com>
2022-07-15 12:17:23 +1200

    item name v1 - loop atleast once

---
+++ 01_item_name.py
@@ -0,0 +1,17 @@
+'''
+
+item name v1 - takes the input from a user atleast once and prints
+'''
+
+
+name_list=[]
+
+def item_name(question):
+    response = input(question)
+    name_list.append(response)
+
+
+item_name("Item name: ")
+print(name_list)
+
+
