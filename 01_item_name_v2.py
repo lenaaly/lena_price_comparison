bbf42d7f9c94581e87ad4825e60a2d79c1763bbf
lenaaly <101528160+lenaaly@users.noreply.github.com>
2022-07-15 12:25:37 +1200

    item name v2 - loop more than once

---
+++ 01_item_name_v2.py
@@ -0,0 +1,28 @@
+'''
+
+item name v1 - takes the input from a user atleast once and prints
+item name v2 - loops more than once and appends to name list
+
+'''
+
+
+name_list=[]
+
+
+def item_name(question):
+    response = input(question)
+
+    while response != "xxx":
+        name_list.append(response)
+
+        if response == "xxx":
+            return response
+        else:
+            response = input(question)
+
+
+
+item_name("Item name: ")
+print(name_list)
+
+
