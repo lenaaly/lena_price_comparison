4732b335273e00be1b8b23b9c5c5f3e7ec8597f6
lenaaly <101528160+lenaaly@users.noreply.github.com>
2022-07-15 12:45:12 +1200

    not blank v1 - after blank input, loop again boolean used

--- 02_not_blank.py
+++ 02_not_blank.py
@@ -7,7 +7,10 @@ not blank - making sure the input is not blank
 def not_blank(question):
     response = input(question)
 
-    while response != "":
+    valid = False
+    while not valid:
+        response = input(question)
+
         if response == "":
             return response
         else:
