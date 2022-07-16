b2838927a7e5a1169375cdfdcba07ab10e74ef19
lenaaly <101528160+lenaaly@users.noreply.github.com>
2022-07-17 11:37:52 +1200

    base v2 - adding item name and reusable not blank functions to base, refining them in base to work

--- 00_base_v2.py
+++ 00_base_v2.py
@@ -12,36 +12,39 @@ base v2 - setting up functions: not blank component and item name component
 
 # setting up functions*****
 
-# item_info=[[name_list], [price_list], [weight_list]]
-
-
-# item name function, takes items name and appends to a list
 
 name_list = []
 
+
+# item_info = [[name_list], [price_list], [weight_list]]
+
+# no blank function, receives question and the error message and checks if question input is blank
+def not_blank(question, error_msg):
+    valid = False
+    # if the question is blank then return as False
+    if question == "":
+        print(error_msg)
+        valid = False
+    # if the input is blank then show error message and return as false so calling function can act on it (will ask again)
+    else:
+        valid = True
+    return valid
+
+
+# item name function, takes items name and appends to a list
+# Get the item name from the user and check if blank, and return the result
 def item_name(question):
     response = input(question)
 
     while response != "xxx":
         name_list.append(response)
-
         if response == "xxx":
             return response
         else:
+            not_blank(response, "This cant be blank - enter the items name")
             response = input(question)
 
-# not blank function, makes sure input is not blank
-def not_blank(question, error_msg):
 
-    valid = False
-
-    while not valid:
-        response = input(question)
-
-        if response != "":
-            return response
-        else:
-            print(error_msg)
 
 
 # item price function, takes item price and appends to a list
@@ -54,13 +57,9 @@ def not_blank(question, error_msg):
 
 # *** MAIN ROUTINE ***
 
-# asking the user the item name
-not_blank("Item name: ", "This cannot be blank - Please enter the Items name.")
-item_name("Item name: ")
-
-
-#print(name_list)
-
 # asking the user what their budget is
 
 # asking the user the names of their items
+item_name("Item name: ")
+print(name_list)
+
