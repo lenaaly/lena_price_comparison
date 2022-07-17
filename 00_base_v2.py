3500040d64d1faf36cd433bd6d051e1db8eb376f
lenaaly <101528160+lenaaly@users.noreply.github.com>
2022-07-17 12:15:05 +1200

    base v2 - refining item name function to call in not blank effectively

--- 00_base_v2.py
+++ 00_base_v2.py
@@ -12,7 +12,6 @@ base v2 - setting up functions: not blank component and item name component
 
 # setting up functions*****
 
-
 name_list = []
 
 
@@ -31,21 +30,22 @@ def not_blank(question, error_msg):
     return valid
 
 
-# item name function, takes items name and appends to a list
+# item name function, takes valid items name and appends to a list
 # Get the item name from the user and check if blank, and return the result
 def item_name(question):
     response = input(question)
 
     while response != "xxx":
-        name_list.append(response)
         if response == "xxx":
             return response
-        else:
+
+        elif response == "":
             not_blank(response, "This cant be blank - enter the items name")
             response = input(question)
 
-
-
+        else:
+            name_list.append(response)
+            response = input(question)
 
 # item price function, takes item price and appends to a list
 
@@ -62,4 +62,3 @@ def item_name(question):
 # asking the user the names of their items
 item_name("Item name: ")
 print(name_list)
-
