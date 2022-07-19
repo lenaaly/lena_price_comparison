e18bfdda6310b337bdd1bdee26f125b12654d666
lenaaly <101528160+lenaaly@users.noreply.github.com>
2022-07-19 12:10:42 +1200

    user budget v2 - calls the number checker function

---
+++ 04_user_budget_v2.py
@@ -0,0 +1,18 @@
+"""
+Lena Aly
+
+budget - asks the user for their budget and stores in a variable
+budget v2 - calls the number checker function
+
+"""
+
+
+def user_budget(question):
+    # will call number checker by using validBudget
+    validBudget = False
+
+    while validBudget is False:
+        budget = input(question)
+        validBudget = num_checker(budget, "Error - Please enter a number above 0")
+        if validBudget:
+            return budget
