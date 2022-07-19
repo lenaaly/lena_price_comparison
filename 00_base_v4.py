7664b012dec41eb5fffe8e2dd1ef48082d773336
lenaaly <101528160+lenaaly@users.noreply.github.com>
2022-07-19 13:53:32 +1200

    base v4 - refining code as explained in slide 33 of testing powerpoint

---
+++ 00_base_v4.py
@@ -0,0 +1,37 @@
+"""
+Lena Aly
+
+This is the base component - the program takes the users budget
+and recommends which items are the best value for money, based on
+the weight and price of the item. The program returns the best
+recommended item within the users budget
+
+base v1 - skeleton of program, setting up functions
+base v2 - setting up functions: not blank component and item name component
+base v3 - setting up functions: num checker, budget and item price
+base v4 - major changes to make code more flexible as explained on slide 33 of testing powerpoint
+
+"""
+
+# setting up functions****
+
+# number checker function, checks for valid numbers and invalid (letters / symbols / blank)
+
+# ask user for string function (used in item name)
+
+# ask user for float function (used in budget, item price and item weight)
+
+
+# MAIN ROUTINE*****
+
+# ask user for their budget once
+
+# ask the user for the item name, price and weight
+# calculate unit price
+# append to lists
+
+# print lists
+
+
+
+
