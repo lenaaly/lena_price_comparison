7175c6e31182abe3460424bb79fd6dc97097f527
lenaaly <101528160+lenaaly@users.noreply.github.com>
2022-07-21 23:01:35 +1200

    yes/no v2 - adding keepGoing to stop asking item questions when no is entered

--- 08_yes_no_v2.py
+++ 08_yes_no_v2.py
@@ -8,16 +8,23 @@ yes / no - after each item name is added, asks if the user has any more items an
 
 def yes_no_items(question, error_msg):
     valid = False
+    # keepGoing set to True so that when response is no, the program will stop asking item questions instead of looping
+    keepGoing = True
     while not valid:
         response = input(question)
+
         if response == "yes" or response == "y":
             valid = True
         elif response == "no" or response == "n":
             valid = True
+            keepGoing = False
         else:
             print(error_msg)
 
-    return response
+    return keepGoing
 
 
 ask_more_items = yes_no_items("Do you have any more items?: ", "This value is invalid - Enter yes / no")
+if ask_more_items is False:
+    keepAskingForItems = False
+t
